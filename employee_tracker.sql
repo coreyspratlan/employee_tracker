8c90dad313db1a264f8a7814b860e7927f2ea5e7
Mozart <coreyspratlan@gmail.com>
2020-07-20 14:54:00 -0500

    Created employee_tracker database and the three tables

--- employee_tracker.sql
+++ employee_tracker.sql
@@ -1,8 +1,26 @@
-DROP DATABASE IF EXISTS department_db;
-CREATE DATABASE department_db;
-USE department_db;
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employedd_tracker;
+USE employee_tracker;
 
-CREATE TABLE department {
-id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE department (
+  id INT(30) AUTO_INCREMENT NOT NULL,
+  departmentName VARCHAR (30),
+  PRIMARY KEY (id)
+);
 
-}
+CREATE TABLE role (
+  id INT(30) AUTO_INCREMENT NOT NULL,
+  title VARCHAR (30),
+  salary INT (30),
+  department_id INT (30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INTEGER(30) AUTO_INCREMENT NOT NULL,
+  firstName VARCHAR (30) NOT NULL,
+  lastName VARCHAR (30) NOT NULL,
+  role_id INT (30),
+  manager_id INT (30),
+  PRIMARY KEY (id)
+);
